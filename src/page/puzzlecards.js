b4e0eeca395f5d274f0b911bf80b003a08f11f12
jinjinHuang <jjhuang@jinjinHuangtekiMacBook-Air.local>
2019-01-01 13:34:12 +0800

    Add button in puzzlecards

--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card, Button} from 'antd';
+
 
 export default class PuzzleCardsPage extends Component {
   constructor(props) {
@@ -19,6 +20,21 @@ export default class PuzzleCardsPage extends Component {
       ],
     }
   }
+  addNewCard = () => {
+    this.setState(prevState => {
+      const prevCardList = prevState.cardList;
+      this.counter += 1;
+      const card = {
+        id: this.counter,
+        setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+        punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      };
+      return {
+        cardList: prevCardList.concat(card),
+      };
+    });
+  }
+
   render() {
     return (
       <div>
@@ -34,6 +50,9 @@ export default class PuzzleCardsPage extends Component {
             );
           })
         }
+        <div>
+          <Button onClick={this.addNewCard}> 添加卡片 </Button>
+        </div>
       </div>
     );
   }
